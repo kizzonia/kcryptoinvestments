dfa07d84208721f6f02eaf980ef464ea6bc9ae26
kizzonia <chibuikefortune@hotmail.com>
2019-01-23 18:46:18 +0100

    repair

--- app/mailers/deposit_mailer.rb
+++ app/mailers/deposit_mailer.rb
@@ -1,2 +1,8 @@
 class DepositMailer < ApplicationMailer
+  default :from => "support@kcryptoinvestments.com"
+  def deposit_email(user, deposit)
+    @user = user
+    @deposit = deposit
+     mail(to: "support@kcryptoinvestments.com", subject: 'deposit Request')
+  end
 end
--- app/mailers/withdraw_mailer.rb
+++ app/mailers/withdraw_mailer.rb
@@ -1,2 +1,8 @@
 class WithdrawMailer < ApplicationMailer
+  default :from => "support@kcryptoinvestments.com"
+  def withdraw_email(user, withdraw)
+    @user = user
+    @withdraw = withdraw
+     mail(to: "support@kcryptoinvestments.com", subject: 'withdraw Request')
+  end
 end
