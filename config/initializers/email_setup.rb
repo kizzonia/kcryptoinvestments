5eed47780386db1e4e4d01de76cf4e2699d6717a
kizzonia <chibuikefortune@hotmail.com>
2019-01-24 10:49:18 +0100

    second

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,12 +1,11 @@
 if Rails.env.production?
-ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'kcryptoinvestments.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'kcryptoinvestments.com',
     :authentication => :plain,
-    :enable_starttls_auto => true
   }
+  ActionMailer::Base.delivery_method = :smtp
 end
