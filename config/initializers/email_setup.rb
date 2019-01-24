bb21e7052d745379516bdef4eaba1185b919b346
kizzonia <chibuikefortune@hotmail.com>
2019-01-24 11:27:12 +0100

    second

---
+++ config/initializers/email_setup.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'kcrypto.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
