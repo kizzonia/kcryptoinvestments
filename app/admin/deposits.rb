e1770ea2be45e4739743fd5e32e6cb1069c9dc6b
kizzonia <chibuikefortune@hotmail.com>
2019-01-25 13:40:36 +0100

    approve

--- app/admin/deposits.rb
+++ app/admin/deposits.rb
@@ -6,6 +6,32 @@ ActiveAdmin.register Deposit do
 #
 # or
 #
+permit_params :amount, :payment_type, :email,  :status, :user_id
+#
+# or
+#
+form do |f|
+
+ f.inputs do
+   f.input :user_id, :label => 'Users', :as => :select, :collection => User.all.map{|u| ["#{u.email}", u.id]}
+   f.input :amount
+   f.input :payment_type
+   f.input :email
+   f.input :status
+
+
+ end
+ f.submit :submit
+end
+def display_name
+ self.email
+end
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
